3a69b9756d061236137adf78dad2029d1ec6d7ce
Martin FIlip <kali@kali>
2022-12-06 10:42:22 +0100

    day6 done

---
+++ day6/day6_part_one.py
@@ -0,0 +1,14 @@
+input = open("day6.txt","r")
+
+instructions = input.read()
+number = 0
+condition = True
+
+while condition:     
+   if number+4 <= len(instructions):
+        four_chars = instructions[number] + instructions[number + 1] + instructions[number + 2] + instructions[number + 3]
+        if not len(set(four_chars)) == len(four_chars):
+            number +=1
+        else:
+            print(number + 4)
+            condition = False
---
+++ day6/day6_part_two.py
@@ -0,0 +1,15 @@
+input = open("day6.txt","r")
+
+instructions = input.read()
+number = 0
+condition = True
+
+while condition:     
+   if number+14 <= len(instructions):
+        four_chars = instructions[number] + instructions[number + 1] + instructions[number + 2] + instructions[number + 3] + instructions[number + 4] + instructions[number + 5] + instructions[number + 6] + instructions[number + 7] + instructions[number + 8] + instructions[number + 9] + instructions[number + 10] + instructions[number + 11] + instructions[number + 12] + instructions[number + 13]
+        if not len(set(four_chars)) == len(four_chars):
+            number +=1
+            print(four_chars)
+        else:
+            print(number +14)
+            condition = False
