d170dfeda4ce5424e1fc1f5e4eb2b76db6136eaf
Martin FIlip <kali@kali>
2022-12-06 10:52:04 +0100

    day6 done

--- day6/day6_part_two.py
+++ day6/day6_part_two.py
@@ -7,7 +7,7 @@ condition = True
 while condition:     
    if number+14 <= len(instructions):
         fourteen_chars = instructions[number] + instructions[number + 1] + instructions[number + 2] + instructions[number + 3] + instructions[number + 4] + instructions[number + 5] + instructions[number + 6] + instructions[number + 7] + instructions[number + 8] + instructions[number + 9] + instructions[number + 10] + instructions[number + 11] + instructions[number + 12] + instructions[number + 13]
-        if not len(set(four_chars)) == len(four_chars):
+        if not len(set(fourteen_chars)) == len(fourteen_chars):
             number +=1
         else:
             print(number +14)
